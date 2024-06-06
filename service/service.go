04e8570486796469e270a519603f76bdea842952
Mohd Arif <mohdarif7700@gmail.com>
2024-06-06 19:37:20 +0530

    added list all and get by exoplanet id api's

--- service/service.go
+++ service/service.go
@@ -5,6 +5,7 @@ import (
 	"exoplanet-service/dto"
 	"exoplanet-service/validate"
 	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -26,6 +27,35 @@ func AddExoplanetHandler(w http.ResponseWriter, r *http.Request) {
 	exoplanet.ID = uuid.New().String()
 	exoplanets[exoplanet.ID] = exoplanet
 	w.WriteHeader(http.StatusCreated)
+	err := json.NewEncoder(w).Encode(exoplanet)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func ListExoplanetsHandler(w http.ResponseWriter, r *http.Request) {
+	var list []dto.Exoplanet
+	for _, exoplanet := range exoplanets {
+		list = append(list, exoplanet)
+	}
+
+	err := json.NewEncoder(w).Encode(list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func GetExoplanetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	exoplanet, exists := exoplanets[id]
+	if !exists {
+		http.Error(w, dto.ErrExoplanetNotFound.Error(), http.StatusNotFound)
+		return
+	}
+
 	err := json.NewEncoder(w).Encode(exoplanet)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
