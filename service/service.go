116ae83bb4942cf6fb15999f1f2c905f796aabc5
Mohd Arif <mohdarif7700@gmail.com>
2024-06-06 19:38:41 +0530

    added fuel estimation api

--- service/service.go
+++ service/service.go
@@ -6,7 +6,9 @@ import (
 	"exoplanet-service/validate"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"math"
 	"net/http"
+	"strconv"
 )
 
 var exoplanets = make(map[string]dto.Exoplanet)
@@ -106,4 +108,35 @@ func DeleteExoplanetHandler(w http.ResponseWriter, r *http.Request) {
 	delete(exoplanets, id)
 	w.WriteHeader(http.StatusNoContent)
 	json.NewEncoder(w)
+}
+
+func FuelEstimationHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	crewCapacity, err := strconv.Atoi(r.URL.Query().Get("crewCapacity"))
+	if err != nil || crewCapacity <= 0 {
+		http.Error(w, "Invalid crew capacity", http.StatusBadRequest)
+		return
+	}
+
+	exoplanet, exists := exoplanets[id]
+	if !exists {
+		http.Error(w, dto.ErrExoplanetNotFound.Error(), http.StatusNotFound)
+		return
+	}
+
+	var gravity float64
+	switch exoplanet.Type {
+	case dto.GasGiant:
+		gravity = 0.5 / math.Pow(exoplanet.Radius, 2)
+	case dto.Terrestrial:
+		gravity = exoplanet.Mass / math.Pow(exoplanet.Radius, 2)
+	}
+
+	fuel := float64(exoplanet.Distance) / math.Pow(gravity, 2) * float64(crewCapacity)
+	err = json.NewEncoder(w).Encode(map[string]float64{"fuel": fuel})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
